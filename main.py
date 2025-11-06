25fce4a60b95a436d03de1265821ec46fe7fe286
Igakyyy <muhammadakbernadeem0@gmail.com>
2025-11-06 05:00:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# ---- Main Code ----
+# ---- Main Code (Values changed) ----
 print("------------------------------------------------------------")
 a = Triangle() 
 print(a)
@@ -6,19 +6,19 @@ print(f"Perimeter : {a.perimeter()}")
 print(f"Right Angle Triangle? : {a.isRightAngled()}")
 print("------------------------------------------------------------")
 
-b = Triangle(2)
+b = Triangle(4)
 print(b)
 print(f"Perimeter : {b.perimeter()}")
 print(f"Right Angle Triangle? : {b.isRightAngled()}")
 print("------------------------------------------------------------")
 
-c = Triangle(3, 4)
+c = Triangle(5, 12)
 print(c)
 print(f"Perimeter : {c.perimeter()}")
 print(f"Right Angle Triangle? : {c.isRightAngled()}")
 print("------------------------------------------------------------")
 
-d = Triangle(3, 4, 5)
+d = Triangle(5, 12, 13)
 print(d)
 print(f"Perimeter : {d.perimeter()}")
 print(f"Right Angle Triangle? : {d.isRightAngled()}")
@@ -30,3 +30,28 @@ print(f"Perimeter : {e.perimeter()}")
 print(f"Right Angle Triangle? : {e.isRightAngled()}")
 print("------------------------------------------------------------")
 print(f"No of Triangles: {Triangle.objectCount()}")
+🧮 New Output Example
+mathematica
+Copy code
+------------------------------------------------------------
+Sides of triangle : A = 2.0 , B = 3.0 , C = 4.0
+Perimeter : 9.0
+Right Angle Triangle? : False
+------------------------------------------------------------
+Sides of triangle : A = 4.0 , B = 5.0 , C = 6.0
+Perimeter : 15.0
+Right Angle Triangle? : False
+------------------------------------------------------------
+Sides of triangle : A = 5.0 , B = 12.0 , C = 5.0
+Perimeter : 22.0
+Right Angle Triangle? : False
+------------------------------------------------------------
+Sides of triangle : A = 5.0 , B = 12.0 , C = 13.0
+Perimeter : 30.0
+Right Angle Triangle? : True
+------------------------------------------------------------
+Sides of triangle : A = 5.0 , B = 12.0 , C = 13.0
+Perimeter : 30.0
+Right Angle Triangle? : True
+------------------------------------------------------------
+No of Triangles: 5
